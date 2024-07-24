76a43e01b98fc93b92a80defe0241641a2e37821
AidanHart0602 <aidanhart321@gmail.com>
2024-07-23 17:48:17 -1000

    SmartAI

--- Scripts/AISphereScript.cs
+++ Scripts/AISphereScript.cs
@@ -4,21 +4,59 @@ using UnityEngine;
 using UnityEngine.AI;
 public class AISphereScript : MonoBehaviour
 {
-    // Start is called before the first frame update
     [SerializeField]
     private GameObject[] _wayPoints;
     private NavMeshAgent _agent;
+    private bool _reverse = false;
+    private int _counter;
     void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
+        if (_agent != null)
+        {
+            _agent.destination = _wayPoints[_counter].transform.position;
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (_agent.remainingDistance < .4)
         {
-            _agent.destination = _wayPoints[Random.Range(0, 6)].transform.position;
+            if (_reverse == false)
+            {
+                Normal();
+            }
+            else if (_reverse == true)
+            {
+                Reverse();
+            }
+            _agent.destination = _wayPoints[_counter].transform.position;
+        }
+    }
+
+    void Normal()
+    {
+        if (_counter == _wayPoints.Length - 1)
+        {
+            _reverse = true;
+            _counter--;
+        }
+        else
+        {
+            _counter++;
+        }
+    }
+
+    void Reverse()
+    {
+        if (_counter == 0)
+        {
+            _reverse = false;
+            _counter++;
+        }
+        else
+        {
+            _counter--;
         }
     }
 }
